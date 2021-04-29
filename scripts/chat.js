83159e1988e46e0bf947fd9caec930936e6ef7fc
unknown <ahmedqayr22@gmail.com>
2021-04-29 12:49:44 +0100

    new chatroom

---
+++ scripts/chat.js
@@ -0,0 +1,62 @@
+
+
+class Chatroom {
+    constructor(room, username){
+      this.room = room;
+      this.username = username;
+      this.chatRoom = db.collection('chatRoom');
+    }
+
+    async addChat(messege){
+        //format a chat object 
+        const now = new Date();
+        const chat = {
+            messege,
+            username: this.username,
+            room: this.room,
+            created_at: firebase.firestore.Timestamp.fromDate(now)
+
+        };
+        //save chat doc
+        const response = await this.chatRoom.add(chat);
+        return response;
+    }
+    getchats(callback){
+      this.unsub = this.chatRoom
+      .where('room', '==', this.room)
+      .orderBy('created_at')
+      .onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(change => {
+          if(change.type === 'added'){
+            callback(change.doc.data());
+          }
+        })
+      })
+    }
+    updateName(username){
+      this.username = username;
+    }
+    updateRoom(room){
+      this.room = room;
+      console.log('room updated');
+      if(this.unsub){
+        this.unsub();
+      }
+      
+    }
+}
+
+const chatroom = new Chatroom('general', 'shaun');
+
+chatroom.getchats((data) => {
+  console.log(data);
+});
+
+setTimeout(() => {
+  chatroom.updateRoom('gaming');
+  chatroom.updateName('Ali');
+  chatroom.getchats((data) => {
+    console.log(data);
+  });
+  chatroom.addChat('hello ali');
+}, 3000)
